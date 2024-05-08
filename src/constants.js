65023e042cfca29f07a183877b62e3b904cf6ebf
encarbassot <elgrefa@gmail.com>
2024-05-08 16:01:31 +0200

    v0.0.1

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,11 @@
 export const EMAIL_CODE_EXPIRATION = 10 // minutes
 
-export const FRONTEND_URL = "http://localhost:3001"
-// export const FRONTEND_URL = "https://keymaster.fabrega.cat"
+// export const FRONTEND_URL = "http://localhost:3001"
+export const FRONTEND_URL = "https://keymaster.fabrega.cat"
 
 // export const BACKEND_URL = "http://localhost:3000"
-export const BACKEND_URL = "http://172.30.5.61:3000"
-// export const BACKEND_URL = "https://keymaster.fabrega.cat/api"
+// export const BACKEND_URL = "http://172.30.5.61:3000"
+export const BACKEND_URL = "https://keymaster.fabrega.cat/api"
 
 
 
